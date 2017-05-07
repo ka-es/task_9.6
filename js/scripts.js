8f1d22331db7632096644d1bbee0dc7255bd8385
eileen <eileen@eileen.com>
2017-05-07 02:09:53 +0200

    modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var add = document.getElementById('js-addElem'),
 	list = document.getElementById('js-list');
 
-add.addEventListener('click', function()) {
+add.addEventListener('click', function() {
 	var getTags = document.getElementsByTagName('li'),
 		getTagsLength = getTags.length;
 	list.innerHTML += '<li>item ' + getTagsLength + '</li>';
